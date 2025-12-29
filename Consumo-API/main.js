d1b7025e3270313d7579235059d9c78ea55361c5
Iván Vargas Orjuela <ivanvargas.va@gmail.com>
2025-12-29 11:44:26 -0500

    feat: Manejo de errores en Consumo API

--- Consumo-API/main.js
+++ Consumo-API/main.js
@@ -1,19 +1,58 @@
+
 const boton = document.getElementById('cargar');
 const lista = document.getElementById('lista');
 
-boton.addEventListener( 'click' , async () => {
-   const response = await fetch('https://jsonplaceholder.typicode.com/users');
-   const usuarios = await response.json();
+// boton.addEventListener( 'click' , async () => {
+//    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//    const usuarios = await response.json();
 
-   lista.innerHTML = ``;
-   console.log(response)
-   console.log(usuarios);
+//    lista.innerHTML = ``;
+//    console.log(response)
+//    console.log(usuarios);
 
-   usuarios.forEach(usuario => {
-      const item = document.createElement('li');
-      item.textContent= `
-         Nombre: ${usuario.name} Correo: ${usuario.email}
+//    usuarios.forEach(usuario => {
+//       const item = document.createElement('li');
+//       item.textContent= `
+//          Nombre: ${usuario.name} Correo: ${usuario.email}
+//       `;
+//       lista.appendChild(item);
+//    });
+// });
+
+
+async function obtenerUsuarios() {
+
+   lista.innerHTML = `🧨 Cargando...`;
+
+   try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      
+      if(!response.ok){
+         throw new Error('Error en la petición');
+      }
+
+      const usuarios = await response.json();
+
+      mostrarUsuarios(usuarios);
+
+   } catch (error) {
+      lista.innerHTML = `Falló API ⛔`;
+      console.error('falló la API: ' + error);
+   }
+}
+
+function mostrarUsuarios(usuariosObtenidos){
+console.log(usuariosObtenidos);
+
+   usuariosObtenidos.forEach(usuario => {
+      const enlistado = createElement('li');
+      
+      enlistado.innerHTML = `
+      
+         ${usuario.name} <br> ${usuario.email}
       `;
-      lista.appendChild(item);
+      lista.appendChild(enlistado);
    });
-});
+}
+
+boton.addEventListener( 'click' ,  obtenerUsuarios);
